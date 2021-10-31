d38212086a28fc3be0711af445799d8d7fe8c88f
kkj6670 <jjy7201@gmail.com>
2021-10-31 21:22:58 +0900

    feat: boardViewer 진행중

--- src/components/board/BoardViewer.tsx
+++ src/components/board/BoardViewer.tsx
@@ -1,18 +1,79 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import styled from 'styled-components';
+
 import marked from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css';
 
 import { IParamTypes } from '../../types/common';
 
+const ViewerBox = styled.article`
+  color: ${({ theme }) => theme.textColor};
+  pre {
+    margin: 2rem 0;
+  }
+  h1,
+  h2,
+  h3 {
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+  }
+  
+  h1 {
+    font-size: 3.5rem;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  h3 {
+    font-size: 2.5rem;
+  }
+
+  code {
+    ::-webkit-scrollbar {
+      width: 15px;
+      height: 15px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: ${({ theme }) => theme.scrollColor.thumb};
+      border-radius: 10px;
+      background-clip: content-box;
+      border: 3px solid rgba(255, 255, 255, 0);
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: ${({ theme }) => theme.scrollColor.track};
+    }
+  }
+}
+`;
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight(code, lang) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+    return hljs.highlight(code, { language }).value;
+  },
+  langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+  pedantic: false,
+  gfm: true,
+  breaks: true,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false,
+});
+
 function BoardViewer({ match }: RouteComponentProps<IParamTypes>) {
   const { params } = match;
-  const [text, setText] = useState('');
   const viewerBox = useRef<HTMLDivElement>(null);
 
-  // TODO :: marekd options
-
   useEffect(() => {
-    fetch(`/react-blog/data/board/javascript/${params.fileName}.md`, {
+    fetch(`/react-blog/data/board/${params.menu}/${params.fileName}.md`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -23,13 +84,14 @@ function BoardViewer({ match }: RouteComponentProps<IParamTypes>) {
       } else {
         response.text().then((md) => {
           const box = viewerBox.current;
+          console.log(md);
           if (box !== null) box.innerHTML = marked(md);
         });
       }
     });
-  }, [params.fileName, setText]);
+  }, [params.fileName]);
 
-  return <div ref={viewerBox} />;
+  return <ViewerBox ref={viewerBox} />;
 }
 
 export default BoardViewer;
