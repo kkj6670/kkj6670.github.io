f35d92c1144fe77f674f429c288923d3f440e16a
kkj6670 <48434120+kkj6670@users.noreply.github.com>
2021-11-05 15:17:50 +0900

    chore: boarData.json 자동화 방식 변경

--- scripts/createBoardData.mjs
+++ scripts/createBoardData.mjs
@@ -1,20 +1,21 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-const boardDir = './public/data/board/';
+const BOARD_JSON_DIR = "./public/data/boardData.json";
+const BOARD_MD_DIR = "./public/data/board/";
 
 const boardData = {};
-const folders = fs.readdirSync(boardDir).map((folder) => folder);
+const folders = fs.readdirSync(BOARD_MD_DIR).map((folder) => folder);
 
 folders.forEach((name) => {
-  const dir = `${boardDir}${name}/`;
+  const dir = `${BOARD_MD_DIR}${name}/`;
   const files = fs.readdirSync(dir).map((fileName) => {
     const fileDir = `${dir}${fileName}`;
     const date = fs.statSync(fileDir).mtime;
 
-    const md = fs.readFileSync(fileDir, 'utf8');
+    const md = fs.readFileSync(fileDir, "utf8");
     // TODO :: 다른방식으로 데이터 추출방법
-    const title = md.match(/(BOARD_TITLE\:)(.*)/g)['0'].replace('BOARD_TITLE: ','');
-    const tag = md.match(/(BOARD_TAG\:)(.*)/g)['0'].replace('BOARD_TAG: ','');
+    const title = md.match(/(BOARD_TITLE\:)(.*)/g)["0"].replace("BOARD_TITLE: ", "");
+    const tag = md.match(/(BOARD_TAG\:)(.*)/g)["0"].replace("BOARD_TAG: ", "");
 
     return {
       fileName,
@@ -24,12 +25,25 @@ folders.forEach((name) => {
     };
   });
 
-  if(files.length > 0) {
-    files.sort((a,b) => b.date - a.date);
+  if (files.length > 0) {
+    files.sort((a, b) => b.date - a.date);
     boardData[name] = files;
   }
 });
 
-const boardDataDir = './public/data/boardData.json';
-fs.unlink(boardDataDir, (err) => console.error(err));
-fs.writeFileSync(boardDataDir, JSON.stringify(boardData));
+fs.open(BOARD_JSON_DIR, 'w', function(err, fd) {
+  if(err) throw err;
+  console.log('1. file open');
+
+  let buf = new Buffer(JSON.stringify(boardData));
+  fs.write(fd, buf, 0, buf.length, null, function(err, written, buffer) {
+    if(err) throw err;
+
+    console.log('2. file write');
+    fs.close(fd, function(err) {
+      if(err) throw err;
+
+      console.log('3. file closed');
+    });
+  });
+});
