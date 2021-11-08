e6fb0d108b12bbccde4d653cf359f960a70723a1
kkj6670 <48434120+kkj6670@users.noreply.github.com>
2021-11-08 16:10:50 +0900

    feat: SEO (react-snap)

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import App from './App';
 import BaseProvider from './store/Base';
 import GlobalStyle from './styles/global';
 
-ReactDOM.render(
-  <BaseProvider>
-    <GlobalStyle />
-    <App />
-  </BaseProvider>,
-  document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <BaseProvider>
+      <GlobalStyle />
+      <App />
+    </BaseProvider>,
+    rootElement,
+  );
+} else {
+  render(
+    <BaseProvider>
+      <GlobalStyle />
+      <App />
+    </BaseProvider>,
+    rootElement,
+  );
+}
