20927a76e5e9f383fad0fc4b6c369aac8c07e943
kkj6670 <48434120+kkj6670@users.noreply.github.com>
2021-11-22 10:26:37 +0900

    feat: windowResize Hook

---
+++ src/lib/hooks/useWindowSize.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export default function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    const handleResize = () => {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+}
